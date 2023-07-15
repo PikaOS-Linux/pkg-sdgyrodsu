1c919123388b5c6c178895783b71018f67b51b45
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-07-15 15:59:35 +0300

    first commit

---
+++ main.sh
@@ -0,0 +1,16 @@
+# Clone Upstream
+git clone https://github.com/kmicki/SteamDeckGyroDSU ./sdgyrodsu
+cp -rvf ./debian ./sdgyrodsu
+cd ./sdgyrodsu
+for i in ../patches/*.patch; do patch -Np1 -i $i ;done
+
+# Get build deps
+apt-get build-dep ./ -y
+
+# Build package
+dpkg-buildpackage --no-sign
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
